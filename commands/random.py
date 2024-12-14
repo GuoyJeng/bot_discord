2ce3a8955d1ac742222198c87839f5b84cc83b60
GuoyJeng <guojing2549@gmail.com>
2024-12-14 21:44:53 +0700

    update

--- commands/random.py
+++ commands/random.py
@@ -15,13 +15,19 @@ with open('questions.json', 'r') as file:
 
 async def send_botton(interaction: discord.Interaction):
     view = View()
-    button = Button(label="Click", style=discord.ButtonStyle.primary, custom_id="button1")
+    button = Button(label="Click", style=discord.ButtonStyle.success, custom_id="button1")
     
     async def button_callback(interaction: discord.Interaction):
         question = random.choice(data['questions'])
         await interaction.response.send_message(question['question'], ephemeral=True)
 
+    embed = discord.Embed(
+            title="Random question",
+            description="It will send you a random question.",
+            color=discord.Color.magenta(),
+        )
+
     button.callback = button_callback
     view.add_item(button)
 
-    await interaction.response.send_message("Random question", view=view)
+    await interaction.response.send_message(embed=embed, view=view)
